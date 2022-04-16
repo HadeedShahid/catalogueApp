5fdde2623de09103d3c13feb79b4bbd10792ef8a
hadeed <hadeed.shahid08@gmail.com>
2022-04-16 01:19:21 -0700

    added login button animation

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:testapp/utils/routes.dart';
-class loginPage extends StatelessWidget {
+class loginPage extends StatefulWidget {
   const loginPage({Key? key}) : super(key: key);
-  
+
+  @override
+  State<loginPage> createState() => _loginPageState();
+}
+
+class _loginPageState extends State<loginPage> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -12,7 +19,7 @@ class loginPage extends StatelessWidget {
           children: [
             Image.asset("assets/images/illus_login.png",fit: BoxFit.cover,),
             SizedBox(height: 20,),
-            Text("Welcome",style: TextStyle(fontSize: 28,fontWeight: FontWeight.normal,),),
+            Text("Welcome $name",style: TextStyle(fontSize: 28,fontWeight: FontWeight.normal,),),
             SizedBox(height: 20,),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16,horizontal: 32),
@@ -22,6 +29,12 @@ class loginPage extends StatelessWidget {
                     hintText: "Enter UserName",
                     labelText: "UserName",
                   ),
+                  onChanged: (value){
+                    name = value;
+                    setState(() {
+
+                    });
+                  },
                 ),
                 TextFormField(
                   obscureText: true,
@@ -31,14 +44,34 @@ class loginPage extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 40,),
-                ElevatedButton(onPressed: (){Navigator.pushNamed(context, Routes.homeRoute);},
-                  child: Text("Login"),
-                  style: TextButton.styleFrom(minimumSize: Size(200,35)),
-                )
+                InkWell(
+                  onTap: ()async{
+                    setState(() {
+                      changeButton = true;
+                    });
+                    await Future.delayed(Duration(seconds:1));
+                    Navigator.pushNamed(context, Routes.homeRoute);
+                     },
+                  child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    width: changeButton? 50 : 150,
+                    height: 50,
+                    alignment: Alignment.center,
+                    child: changeButton? Icon(Icons.done,color: Colors.white,) : Text("Login",style: TextStyle(color: Colors.white,fontSize: 17)),
+                    decoration: BoxDecoration(
+                      color: Colors.cyan,
+                      borderRadius: BorderRadius.circular(changeButton?40:8)
+                    ),
+                  ),
+                ),
+                // ElevatedButton(onPressed: (){Navigator.pushNamed(context, Routes.homeRoute);},
+                //   child: Text("Login"),
+                //   style: TextButton.styleFrom(minimumSize: Size(200,35)),
+                // )
               ],
               ),
             ),
-            
+
           ],
         ),
       ),
